8aeec962132e5d496c2884984b0485d122dbd386
AlexVDefi <alexandervdefi@gmail.com>
2025-11-17 22:06:09 +0000

    Translation strings for mod options

--- Contents/mods/HorseMod/42/media/lua/client/HorseMod/ModOptions.lua
+++ Contents/mods/HorseMod/42/media/lua/client/HorseMod/ModOptions.lua
@@ -9,13 +9,13 @@ local config = {
 
 -- TODO: use translation strings for this
 local function HorseConfig()
-    local options = PZAPI.ModOptions:create("HorseMod", "Horse")
+    local options = PZAPI.ModOptions:create("HorseMod", getText("IGUI_ModOptions_HorseModName"))
 
-    options:addDescription("Change the volume of Horse sounds.")
-    config.horseSoundVolume = options:addSlider("HorseSoundVolume", "Horse Sound Volume (Default 0.40)", 0.01, 1, 0.01, 0.40, "Set sound volume of horse sounds.")
+    options:addDescription(getText("IGUI_ModOptions_HorseSoundVolumeDesc"))
+    config.horseSoundVolume = options:addSlider("HorseSoundVolume", getText("IGUI_ModOptions_HorseSoundVolumeName"), 0.01, 1, 0.01, 0.40, getText("IGUI_ModOptions_HorseSoundVolumeTooltip"))
 
-    options:addDescription("Horse keybinds.")
-    config.horseJumpButton = options:addKeyBind("HorseJumpButton", "Horse Jump Button", Keyboard.KEY_SPACE, "Change the keybind for horse jumping.")
+    options:addDescription(getText("IGUI_ModOptions_HorseKeybind"))
+    config.horseJumpButton = options:addKeyBind("HorseJumpButton", getText("IGUI_ModOptions_HorseKeybindJumpName"), Keyboard.KEY_SPACE, getText("IGUI_ModOptions_HorseKeybindJumpTooltip"))
 end
 
 HorseConfig()
