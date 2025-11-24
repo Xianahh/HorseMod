405952006f97aa6e2d5878c63c0b9eccd97693bb
SirDoggyJvla <simon.cadet79@hotmail.fr>
2025-11-24 01:14:02 +0100

    init unequip timed action

---
+++ Contents/mods/HorseMod/42/media/lua/shared/HorseMod/TimedActions/ISHorseUnequipGear.lua
@@ -0,0 +1,54 @@
+---@namespace HorseMod
+
+---REQUIREMENTS
+local Attachments = require("HorseMod/Attachments")
+local ISHorseEquipGear = require("HorseMod/TimedActions/ISHorseEquipGear")
+
+---@class ISHorseUnequipGear : ISHorseEquipGear
+---@field horse IsoAnimal
+---@field oldAccessory InventoryItem
+---@field attachmentDef AttachmentDefinition
+---@field unlockFn fun()?
+local ISHorseUnequipGear = ISHorseEquipGear:derive("ISHorseUnequipGear")
+
+function ISHorseUnequipGear:perform()
+    local horse = self.horse
+    local player = self.character
+    local oldAccessory = self.oldAccessory
+    local attachmentDef = self.attachmentDef
+    local slot = attachmentDef.slot
+
+    -- remove old accessory from slot and give to player or drop
+    Attachments.setAttachedItem(horse, slot, nil)
+    self:giveBackToPlayerOrDrop(player, horse, oldAccessory)
+
+    ---@TODO
+    -- local bySlot, ground = AttachmentUtils.ensureHorseModData(animal)
+    -- bySlot[slot] = nil
+    -- ground[slot] = nil
+
+    if self.unlockFn then self.unlockFn() end
+    ISBaseTimedAction.perform(self)
+end
+
+---@param character IsoGameCharacter
+---@param horse IsoAnimal
+---@param accessory InventoryItem
+---@param unlockFn fun()?
+---@return ISHorseUnequipGear
+---@nodiscard
+function ISHorseUnequipGear:new(character, horse, accessory, unlockFn)
+    local o = ISHorseEquipGear:new(character, horse, accessory, unlockFn) --[[@as ISHorseUnequipGear]]
+    o.horse   = horse
+    o.oldAccessory = accessory
+    local attachmentDef = Attachments.getAttachmentDefinition(accessory:getFullType())
+    o.maxTime = attachmentDef.unequipTime or 90
+    o.attachmentDef = attachmentDef
+    o.unlockFn = unlockFn
+    o.stopOnWalk = true
+    o.stopOnRun  = true
+    o.stopOnAim  = true
+    return o
+end
+
+return ISHorseUnequipGear
